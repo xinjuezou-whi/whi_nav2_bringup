882e681752807c12564eb76c80e394574b6e5001
Xinjue Zou <hit_zou@126.com>
2025-07-24 08:41:36 +0800

    create new repo

---
+++ launch/mapping_slam_toolbox.py
@@ -0,0 +1,123 @@
+# Copyright 2025 WheelHub Intelligent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
+
+def generate_launch_description():
+    # Input parameters declaration
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    vehicle = LaunchConfiguration("vehicle")
+    vehicle_model = LaunchConfiguration("vehicle_model")
+
+    # Getting directories and launch-files
+    whi_motion_hw_if_launch_file = PathJoinSubstitution([
+        FindPackageShare('whi_motion_hw_interface'),
+        'launch',
+        'bringup.py'
+    ])
+    lidar_lakibeam1_launch_file = PathJoinSubstitution([
+        FindPackageShare('lakibeam1'),
+        'launch',
+        'lakibeam1_scan.launch.py'
+    ])
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("whi_navi2_bringup"), "launch", "config_mapping.rviz"]
+    )
+    slam_toolbox_config_file = PathJoinSubstitution(
+        [FindPackageShare("slam_toolbox"), "config", "mapper_params_online_async.yaml"]
+    )
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+
+    # Nodes launching commands
+    start_whi_motion_hw_if_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(lidar_lakibeam1_launch_file),
+        launch_arguments={
+            'vehicle': vehicle,
+            'vehicle_model': vehicle_model,
+        }.items()
+    )
+
+    # LiDAR
+    start_lakibeam1_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(whi_motion_hw_if_launch_file),
+        launch_arguments={
+            'frame_id': 'laser',
+            'output_topic0': 'scan',
+        }.items()
+    )
+
+    slam_toolbox_params = RewrittenYaml(
+        source_file=slam_toolbox_config_file,
+        root_key=namespace,
+        param_rewrites={
+            'use_sim_time': use_sim_time,
+            'odom_frame': 'odom',
+            'base_frame': 'base_link',
+            'scan_topic': 'scan',
+            'resolution': '0.05',
+        },
+        convert_types=True)
+    start_slam_toolbox_cmd = Node(
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[slam_toolbox_params]
+    )
+
+    # rviz visualization
+    start_rviz_cmd = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_file],
+    )
+
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # Running whi_motion_hw_if
+    ld.add_action(start_whi_motion_hw_if_cmd)
+    # Running LiDAR
+    ld.add_action(start_lakibeam1_cmd)
+
+    # Running SLAM Toolbox
+    ld.add_action(start_slam_toolbox_cmd)
+
+    # rviz
+    ld.add_action(start_rviz_cmd)
+
+    return ld
