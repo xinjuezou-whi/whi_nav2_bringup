7686ad774b497cf2903402f7b9af3ee6d79e8ed0
Xinjue <hit_zou@126.com>
2025-08-27 11:00:22 +0800

    update params for cartographer

--- config/nav2_lds_2d_ekf.lua
+++ config/nav2_lds_2d_ekf.lua
@@ -49,14 +49,13 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.12
 TRAJECTORY_BUILDER_2D.max_range = 30.0
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.0
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.15)
 
-POSE_GRAPH.constraint_builder.min_score = 0.65
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
-
--- POSE_GRAPH.optimize_every_n_nodes = 0
+POSE_GRAPH.constraint_builder.min_score = 0.55
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.65
+POSE_GRAPH.optimize_every_n_nodes = 60
 
 return options
