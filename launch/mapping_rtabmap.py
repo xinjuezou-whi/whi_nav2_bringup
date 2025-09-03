b5fbb06553f093c44edf68731235c5d53aed437a
Xinjue <hit_zou@126.com>
2025-09-03 16:50:45 +0800

    integrate ipc odom for rtabmap

--- launch/mapping_rtabmap.py
+++ launch/mapping_rtabmap.py
@@ -16,6 +16,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 import subprocess
@@ -54,6 +55,7 @@ def launch_setup(context, *args, **kwargs):
     vehicle = LaunchConfiguration("vehicle").perform(context)
     vehicle_model = LaunchConfiguration("vehicle_model").perform(context)
     use_ekf = LaunchConfiguration("use_ekf").perform(context)
+    enable_odom = LaunchConfiguration("enable_odom").perform(context)
 
     # Getting directories and launch-files
     whi_motion_hw_if_launch_file = PathJoinSubstitution([
@@ -76,7 +78,8 @@ def launch_setup(context, *args, **kwargs):
         launch_arguments={
             'vehicle': vehicle,
             'vehicle_model': vehicle_model,
-            'use_ekf': use_ekf
+            'use_ekf': use_ekf,
+            'enable_odom': enable_odom
         }.items()
     )
 
@@ -89,6 +92,106 @@ def launch_setup(context, *args, **kwargs):
     )
 
     # rtab map
+    start_rtabmap_odom_cmd = Node(
+        package='rtabmap_odom', executable='icp_odometry', output='screen',
+        parameters=[{
+            'frame_id': 'laser_multi', # align with rslidar's config
+            'odom_frame_id': 'odom',
+            'wait_for_transform': 0.2,
+            'expected_update_rate': 15.0,
+            'deskewing': False,
+            'use_sim_time': use_sim_time,
+        }],
+        remappings=[
+            ('scan_cloud', '/rslidar_points')
+        ],
+        arguments=[
+            'Icp/PointToPlane', 'true',
+            'Icp/Iterations', '10',
+            'Icp/VoxelSize', '0.1',
+            'Icp/Epsilon', '0.001',
+            'Icp/PointToPlaneK', '20',
+            'Icp/PointToPlaneRadius', '0',
+            'Icp/MaxTranslation', '2',
+            'Icp/MaxCorrespondenceDistance', '1',
+            'Icp/Strategy', '1',
+            'Icp/OutlierRatio', '0.7',
+            'Icp/CorrespondenceRatio', '0.01',
+            'Odom/ScanKeyFrameThr', '0.4',
+            'OdomF2M/ScanSubtractRadius', '0.1',
+            'OdomF2M/ScanMaxSize', '15000',
+            'OdomF2M/BundleAdjustment', 'false',
+        ],
+        condition=UnlessCondition(LaunchConfiguration("enable_odom")),
+    )
+            
+    start_rtabmap_util_cmd = Node(
+        package='rtabmap_util', executable='point_cloud_assembler', output='screen',
+        parameters=[{
+            'max_clouds': 10,
+            'fixed_frame_id': '',
+            'use_sim_time': use_sim_time,
+        }],
+        remappings=[
+            ('cloud', 'odom_filtered_input_scan')
+        ],
+        condition=UnlessCondition(LaunchConfiguration("enable_odom")),
+    )
+
+    start_rtabmap_slam_ipc_cmd = Node(
+        package='rtabmap_slam', executable='rtabmap', output='screen',
+        parameters=[{
+            'frame_id': 'laser_multi', # align with rslidar's config
+            'subscribe_depth': False,
+            'subscribe_rgb': False,
+            'subscribe_scan_cloud': True,
+            'approx_sync': False,
+            'wait_for_transform': 0.2,
+            'use_sim_time': use_sim_time,
+        }],
+        remappings=[
+            ('scan_cloud', 'assembled_cloud')
+        ],
+        arguments=[
+            '-d', # This will delete the previous database (~/.ros/rtabmap.db)
+            'Grid/3D', 'false',
+            'Grid/CellSize', '0.1',
+            'Grid/RayTracing', 'true',
+            'Grid/RayTracingRange', '6.0',
+            'Grid/ClusterRadius', '0.1',
+            'Grid/MinClusterSize', '20',
+            'Grid/ScanVoxelSize', '0.05',
+            'Grid/RangeMin', '0.6',
+            'Grid/RangeMax', '80.0',
+            'Grid/MaxGroundHeight', '0.2',
+            'Grid/MinGroundHeight', '-0.2',
+            'Grid/FlatObstacleDetected', 'true',
+            'Reg/Force3DoF', 'true',
+            'Optimizer/Iterations', '30',
+            'RGBD/ProximityMaxGraphDepth', '0',
+            'RGBD/ProximityPathMaxNeighbors', '1',
+            'RGBD/AngularUpdate', '0.05',
+            'RGBD/LinearUpdate', '0.05',
+            'RGBD/CreateOccupancyGrid', 'false',
+            'Mem/NotLinkedNodesKept', 'false',
+            'Mem/STMSize', '30',
+            'Mem/LaserScanNormalK', '20',
+            'Reg/Strategy', '1',
+            'Icp/VoxelSize', '0.1',
+            'Icp/PointToPlaneK', '20',
+            'Icp/PointToPlaneRadius', '0',
+            'Icp/PointToPlane', 'true',
+            'Icp/Iterations', '10',
+            'Icp/Epsilon', '0.001',
+            'Icp/MaxTranslation', '3',
+            'Icp/MaxCorrespondenceDistance', '1',
+            'Icp/Strategy', '1',
+            'Icp/OutlierRatio', '0.7',
+            'Icp/CorrespondenceRatio', '0.2',
+        ],
+        condition=UnlessCondition(LaunchConfiguration("enable_odom")),
+    )
+
     start_rtabmap_slam_cmd = Node(
         package='rtabmap_slam', executable='rtabmap', output='screen',
         parameters=[{
@@ -122,6 +225,7 @@ def launch_setup(context, *args, **kwargs):
         remappings=[
             ('scan_cloud', '/rslidar_points')
         ],
+        condition=IfCondition(LaunchConfiguration("enable_odom")),
     )
 
     # map server
@@ -157,6 +261,9 @@ def launch_setup(context, *args, **kwargs):
     launch_nodes = [
         start_whi_motion_hw_if_cmd,
         start_rslidar_cmd,
+        start_rtabmap_odom_cmd,
+        start_rtabmap_util_cmd,
+        start_rtabmap_slam_ipc_cmd,
         start_rtabmap_slam_cmd,
         start_map_saver_server_cmd,
         start_lifecycle_manager_cmd,
@@ -182,5 +289,7 @@ def generate_launch_description():
             description='Use ekf to fuse localization'),
         DeclareLaunchArgument('deskewing', default_value='false',
             description='Enable lidar deskewing'),
+        DeclareLaunchArgument('enable_odom', default_value='true',
+            description='whether to publish odom'),
         OpaqueFunction(function=launch_setup)
     ])
